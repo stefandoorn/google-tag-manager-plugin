4a737429587fd31719f034a32d4f2eaa48a834ed
Stefan Doorn <stefan@efectos.nl>
2017-04-27 16:20:19 +0200

    Add basic tests for listeners

---
+++ tests/EventListener/AddRouteListenerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\GtmPlugin\EventListener;
+
+use GtmPlugin\EventListener\AddRouteListener;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Xynnn\GoogleTagManagerBundle\Service\GoogleTagManager;
+
+/**
+ * Class AddRouteListenerTest
+ * @package Tests\GtmPlugin\EventListener
+ * @covers \GtmPlugin\EventListener\AddRouteListener
+ */
+class AddRouteListenerTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testAddRouteIsAddedToGtmObject()
+    {
+        $request = new Request(['_route' => 'test_route']);
+        $requestStack = new RequestStack();
+        $requestStack->push($request);
+
+        $gtm = new GoogleTagManager(true, 'id1234');
+        $listener = new AddRouteListener(true, $gtm, $requestStack);
+        $mock = $this->getMockBuilder(GetResponseEvent::class)->disableOriginalConstructor()->getMock();
+        $listener->onKernelRequest($mock);
+
+        $this->assertArrayHasKey('route', $gtm->getData());
+        $this->assertSame($gtm->getData()['route'], 'test_route');
+    }
+
+    public function testAddRouteIsNotAddedToGtmObject()
+    {
+        $request = new Request(['_route' => 'test_route']);
+        $requestStack = new RequestStack();
+        $requestStack->push($request);
+
+        $gtm = new GoogleTagManager(true, 'id1234');
+        $listener = new AddRouteListener(false, $gtm, $requestStack);
+        $mock = $this->getMockBuilder(GetResponseEvent::class)->disableOriginalConstructor()->getMock();
+        $listener->onKernelRequest($mock);
+
+        $this->assertArrayNotHasKey('route', $gtm->getData());
+    }
+}
---
+++ tests/EventListener/EnvironmentListenerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\GtmPlugin\EventListener;
+
+use GtmPlugin\EventListener\EnvironmentListener;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Xynnn\GoogleTagManagerBundle\Service\GoogleTagManager;
+
+/**
+ * Class EnvironmentListenerTest
+ * @package Tests\GtmPlugin\EventListener
+ * @covers \GtmPlugin\EventListener\EnvironmentListener
+ */
+class EnvironmentListenerTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testEnvironmentIsAddedToGtmObject()
+    {
+        $gtm = new GoogleTagManager(true, 'id1234');
+        $listener = new EnvironmentListener(true, $gtm, 'test_env');
+        $mock = $this->getMockBuilder(GetResponseEvent::class)->disableOriginalConstructor()->getMock();
+        $listener->onKernelRequest($mock);
+
+        $this->assertArrayHasKey('env', $gtm->getData());
+        $this->assertSame($gtm->getData()['env'], 'test_env');
+    }
+
+    public function testEnvironmentIsNotAddedToGtmObject()
+    {
+        $gtm = new GoogleTagManager(true, 'id1234');
+        $listener = new EnvironmentListener(false, $gtm, 'test_env');
+        $mock = $this->getMockBuilder(GetResponseEvent::class)->disableOriginalConstructor()->getMock();
+        $listener->onKernelRequest($mock);
+
+        $this->assertArrayNotHasKey('env', $gtm->getData());
+    }
+}
