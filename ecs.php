25f5bc887ac8023a3f03d5dfbd17c8b845d1adb7
Stefan Doorn <stefan@efectos.nl>
2023-03-23 13:23:24 +0100

    ECS improvements

--- ecs.php
+++ ecs.php
@@ -1,8 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\ControlStructure\TrailingCommaInMultilineFixer;
+use PhpCsFixer\Fixer\FunctionNotation\NativeFunctionInvocationFixer;
 use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
 use PhpCsFixer\Fixer\Operator\BinaryOperatorSpacesFixer;
-use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return static function (ECSConfig $config): void {
     $config->import('vendor/sylius-labs/coding-standard/ecs.php');
@@ -13,4 +16,12 @@ return static function (ECSConfig $config): void {
     ]);
 
     $config->ruleWithConfiguration(BinaryOperatorSpacesFixer::class, []);
+
+    $services = $config->services();
+    $services->set(
+        NativeFunctionInvocationFixer::class
+    )->call('configure', [['include' => ['@all'], 'scope' => 'all', 'strict' => \true]]);
+    $services->set(
+        TrailingCommaInMultilineFixer::class
+    )->call('configure', [['elements' => ['arrays']]]);
 };
